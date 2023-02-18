197d142d2beb3a857425c1a80fff8e77a439ee4d
EduardKurbanov <edward89@ukr.net>
2023-02-18 19:01:25 +0200

    test41

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,6 +78,17 @@ namespace :deploy do
 #  end
 
 #  after 'deploy:seed'
+
+task :seed do
+    puts "\n=== Seeding Database ===\n"
+    on primary :db do
+     within current_path do
+       with rails_env: fetch(:stage) do
+         execute :rake, 'db:seed'
+       end
+     end
+    end
+   end
 end
 
 after 'deploy:finished', 'nginx:restart'

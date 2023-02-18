96b69e62c459221fe7d0a01c095bf130ee12ae57
EduardKurbanov <edward89@ukr.net>
2023-02-18 17:16:21 +0200

    test23

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-AdminUser.create!(email: 'admin@ukr.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@ukr.com', password: 'password', password_confirmation: 'password')
 
 module OnlineStore
   class Application < Rails::Application

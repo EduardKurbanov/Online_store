265add72f3bb97bcf5b60e24734af3e2f5e399f6
EduardKurbanov <edward89@ukr.net>
2023-02-18 17:58:00 +0200

    test30

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,6 +66,21 @@ namespace :deploy do
   task :seed do
     run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
   end
+  load 'lib/deploy/seed'
+
+
+  desc 'Runs rake db:seed for SeedMigrations data'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
+
+  after 'deploy:migrate', 'deploy:seed'
 end
 
 after 'deploy:finished', 'nginx:restart'

90f8c5d5b97a2abeed0c4051367562eae7444a0e
EduardKurbanov <edward89@ukr.net>
2023-02-18 18:43:06 +0200

    test36

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,18 +66,18 @@ namespace :deploy do
 
 
 
-#  desc 'Runs rake db:seed for SeedMigrations data'
-#  task :seed => [:set_rails_env] do
-#    on primary fetch(:migration_role) do
-#      within release_path do
-#        with rails_env: fetch(:rails_env) do
-#          execute :rake, "db:seed"
-#        end
-#      end
-#    end
-#  end
+  desc 'Runs rake db:seed for SeedMigrations data'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
 
-  #after 'deploy:migrate', 'deploy:seed'
+  after 'deploy:migrate', 'deploy:seed'
 end
 
 after 'deploy:finished', 'nginx:restart'
